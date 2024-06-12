7106f15b090c1a4fa479871fa53d520b4b817efe
KennetSoto <kennet.soto@hotmail.com>
2024-06-12 20:30:28 -0300

    v 1.0

--- autogit/main.py
+++ autogit/main.py
@@ -22,19 +22,48 @@ auto.write('git init')
 auto.press('enter')
 
 #usuário insira o nome do arquivo
-auto.write('read -p "Digite o nome do arquivo: " arquivo')
+auto.write('read -p "Digite o nome do arquivo e aperte enter: " arquivo')
 auto.press('enter')
 
-time.sleep(10)
+time.sleep(14)
 auto.write('touch "$arquivo"')
 auto.press('enter')
 
 auto.write('git add .')
 auto.press('enter')
 
-auto.write('read -p "Digite o nome do usuario: "usuario')
+#pede nome do usuario para configuração
+auto.write('read -p "Digite o nome do usuario e aperte enter: " usuario')
 auto.press('enter')
 
-time.sleep(10)
+time.sleep(14)
 auto.write('git config --local user.name "$usuario"')
+auto.press('enter')
+
+#pede email do usuario para configuração
+auto.write('read -p "Digite o email e aperte enter: " email')
+auto.press('enter')
+
+time.sleep(20)
+auto.write('git config --local user.email "$email"')
+auto.press('enter')
+
+#pede nome comit para configuração
+auto.write('read -p "Digite o nome do comit e aperte enter: " comit')
+auto.press('enter')
+
+time.sleep(14)
+auto.write('git commit -m "$comit"')
+auto.press('enter')
+
+#pede o comando completo para adicionar o repositorio remoto
+auto.write('read -p "Cole o comando completo para adicionar o repositorio remoto e aperte enter: " repo')
+auto.press('enter')
+
+time.sleep(14)
+auto.write('$repo')
+auto.press('enter')
+
+#digita o endereço do repositorio
+auto.write('git push -u origin main')
 auto.press('enter')
